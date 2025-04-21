9702c11c10cba2662401d7e84fcfdbedd5c745c3
mohit <mohitratan2003@gmail.com>
2025-04-21 18:00:04 +0530

    Added cors

--- lib/api.ts
+++ lib/api.ts
@@ -7,10 +7,6 @@ const API_BASE_URL =
 
 const api = axios.create({
   baseURL: API_BASE_URL,
-  withCredentials: true,
-  headers: {
-    'Content-Type': 'application/json',
-  }
 });
 
 //user login
